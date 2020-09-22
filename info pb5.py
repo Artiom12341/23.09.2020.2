1347e5f9c344bfa51e6c16790c403be02d1b9aee
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-09-22 19:53:59 +0300

    Update info pb5.py

--- info pb5.py
+++ info pb5.py
@@ -1,9 +1,9 @@
 n=5 
-#metri in centimetri
+# n metri in centimetri
 print (n*100) 
-#kilograme in miligrame
+# n kilograme in miligrame
 print(n*10000)
-"""ani in luni ,saptamani,zile""" 
+"""n ani in luni ,saptamani,zile""" 
 print(n *12)
 print(n*54)
 print(n*365)  
